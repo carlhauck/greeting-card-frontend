58cde23e6106c69616b1c5422627fbf5c11cd2eb
carlhauck <carlhauck@gmail.com>
2020-11-16 20:46:12 -0600

    initial commit (w/ card generator component that uses hooks instead of class-based component)

---
+++ src/components/CardGenerator.js
@@ -0,0 +1,67 @@
+import React, { useState, useEffect } from "react"
+
+function CardGenerator() {
+  const [state, setState] = useState({
+    topText: "",
+    bottomText: "",
+    randomImg: "http://i.imgflip.com/1bij.jpg",
+    allMemeImgs: []
+  })
+
+  useEffect(() => {
+    fetch("https://api.imgflip.com/get_memes")
+      .then(response => response.json())
+      .then(response => {
+        const { memes } = response.data
+        setState({
+          ...state,
+          allMemeImgs: memes
+        });
+      })
+  }, [])
+
+  function handleChange(e) {
+    const value = e.target.value;
+    setState({
+      ...state,
+      [e.target.name]: value
+    });
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault()
+    const randNum = Math.floor(Math.random() * state.allMemeImgs.length)
+    const randMemeImg = state.allMemeImgs[randNum].url
+    setState({ ...state, randomImg: randMemeImg })
+  }
+
+  return (
+    <div>
+      <form className="meme-form" onSubmit={handleSubmit}>
+        <input
+          type="text"
+          name="topText"
+          placeholder="Top Text"
+          value={state.topText}
+          onChange={handleChange}
+        />
+        <input
+          type="text"
+          name="bottomText"
+          placeholder="Bottom Text"
+          value={state.bottomText}
+          onChange={handleChange}
+        />
+
+        <button>Gen</button>
+      </form>
+      <div className="meme">
+        <img src={state.randomImg} alt="" />
+        <h2 className="top">{state.topText}</h2>
+        <h2 className="bottom">{state.bottomText}</h2>
+      </div>
+    </div>
+  )
+}
+
+export default CardGenerator
