0cb37b244a7eb937ff9929a6c09e9f7f796e0cd2
carlhauck <carlhauck@gmail.com>
2020-11-28 00:43:13 -0600

    breaks elements out into components, adds a few svg icons, and does a bit of crude styling

--- src/components/CardGenerator.js
+++ src/components/CardGenerator.js
@@ -1,4 +1,6 @@
 import React, { useState, useEffect } from "react"
+import TextEditor from "./TextEditor"
+import IconRotate from "./IconRotate"
 
 function CardGenerator() {
   const [state, setState] = useState({
@@ -40,48 +42,40 @@ function CardGenerator() {
   }
 
   return (
-    <div>
-      <form className="meme-form" onSubmit={handleSubmit}>
-        <input
-          type="text"
-          name="topText"
-          placeholder="Top Text"
-          value={state.topText}
-          onChange={handleChange}
-        />
-        <label for="topFont">Choose a top font:</label>
-        <select name="topFont" id="topFont" value={state.topFont} onChange={handleChange}>
-          <option value="lora">Lora</option>
-          <option value="mountains">Mountains of Christmas</option>
-        </select>
-        <input type="color" id="topFontColor" name="topFontColor"
-          value={state.topFontColor} onChange={handleChange} />
-        <label for="topFontColor">Top Font Color</label>
+    <div className="container">
+      <div className="item" style={{ justifyItems: "center" }}>
+        <p className="section-header">image</p>
+        <form className="meme-form" onSubmit={handleSubmit}>
+          <button className="img-button">
+            <IconRotate />
+          </button>
+        </form>
+      </div>
 
-        <input
-          type="text"
-          name="bottomText"
-          placeholder="Bottom Text"
-          value={state.bottomText}
-          onChange={handleChange}
-        />
-        <label for="bottomFont">Choose a bottom font:</label>
-        <select name="bottomFont" id="bottomFont" value={state.bottomFont} onChange={handleChange}>
-          <option value="lora">Lora</option>
-          <option value="mountains">Mountains of Christmas</option>
-        </select>
-        <input type="color" id="bottomFontColor" name="bottomFontColor"
-          value={state.bottomFontColor} onChange={handleChange} />
-        <label for="bottomFontColor">Bottom Font Color</label>
-
-        <button>Gen</button>
-      </form>
       <div className="meme">
         <img src={state.randomImg} alt="" />
         <h2 className={`top ${state.topFont}`} style={{ color: `${state.topFontColor}` }}>{state.topText}</h2>
         <h2 className={`bottom ${state.bottomFont}`} style={{ color: `${state.bottomFontColor}` }}>{state.bottomText}</h2>
       </div>
-    </div>
+
+      <div className="item">
+        <p className="section-header">text</p>
+        <TextEditor
+          locationY="top"
+          text={state.topText}
+          font={state.topFont}
+          fontColor={state.topFontColor}
+          handleChange={handleChange} />
+
+        <TextEditor
+          locationY="bottom"
+          text={state.bottomText}
+          font={state.bottomFont}
+          fontColor={state.bottomFontColor}
+          handleChange={handleChange} />
+      </div>
+
+    </div >
   )
 }
 
