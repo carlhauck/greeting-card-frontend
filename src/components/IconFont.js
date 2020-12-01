fae0f9969762a24b685320771242633483dbf0fd
hubert <huby40@gmail.com>
2020-11-30 21:58:21 -0500

    adds styling to text editor

--- src/components/IconFont.js
+++ src/components/IconFont.js
@@ -2,7 +2,7 @@ import React from "react"
 
 function IconFont() {
   return (
-    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16"><title>text</title><g fill="#000000"><polygon fill="#111111" points="0,0 0,6 1,6 3,3 6,3 6,13 3,15 3,16 13,16 13,15 10,13 10,3 13,3 15,6 16,6 16,0 "></polygon></g></svg>
+    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16"><title>text</title><g fill="#000000"><polygon fill="#30659B" points="0,0 0,6 1,6 3,3 6,3 6,13 3,15 3,16 13,16 13,15 10,13 10,3 13,3 15,6 16,6 16,0 "></polygon></g></svg>
   )
 }
 
